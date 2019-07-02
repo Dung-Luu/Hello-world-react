76775f0062a36d5a5c8ca2a444df9440d56bcfff
Thuy Dung <dung.luu4007@gmail.com>
2019-07-02 07:54:23 +0700

    First commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>I just created my first React App</p>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello-world-react
+# Hello-world-react
